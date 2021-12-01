b740ab56aacb26e9978f4b7119712ee782ae54d2
DmytroTiutiunnyk <dmitriyborec93@gmail.com>
2021-12-01 17:10:58 +0200

    initial commit

--- index.php
+++ index.php
@@ -1,9 +1,17 @@
+<html>
+<head>
+    <title>Задача 3</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+
 <?php
+if (1 == 1) {
+    echo "Привіт, світ!";
+} else {
+    echo "Добрий ранок, світ!";
+}
+?>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-echo "Hello World";
+</body>
+</html>
